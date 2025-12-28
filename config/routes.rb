897e192af8190e655efb6e75b4fed5f71ca266f0
Patrick Pang <pang30571@gmail.com>
2025-12-28 09:29:50 +0800

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  # get "home/index"
+  root "home#index"
+  get "home/about"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+end
